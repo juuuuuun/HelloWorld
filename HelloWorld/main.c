36add3a421e3352067be1e943c52c755159b8fa3
Jun Oh <kyungjunjoh@gmail.com>
2018-12-24 00:13:35 -0800

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Jun Oh on 2018-12-24.
+//  Copyright © 2018 Jun Oh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
